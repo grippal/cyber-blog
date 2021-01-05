a1f426e7189a2ef4bce86f88ca8b5cad272e5e6e
grippal <lucagrip@yahoo.com>
2021-01-04 23:58:11 -0500

    basic website dark mode almost complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./_site/**/*.html'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'roboto': ['Roboto', 'sans-serif'],
+      'roboto-mono': ['Roboto\\ mono', 'sans-serif']
+    },
     extend: {},
   },
   variants: {
